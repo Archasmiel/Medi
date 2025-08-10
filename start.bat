3d72e9edbffc7d519fde0186a5436982feca7a89
Archasmiel <archasmiel@gmail.com>
2025-08-11 02:34:16 +0300

    Launching script (windows)

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+REM ============================
+REM  Flask Portable App Starter
+REM ============================
+
+REM Change to script directory
+cd /d "%~dp0"
+
+REM Run Python file with portable Python
+python-win\python.exe run.py
